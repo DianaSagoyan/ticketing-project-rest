ca5f52fa4987a01551f06c45794b5487110457a5
DianaSagoyan <dianasagoyan@yahoo.com>
2022-12-06 14:03:19 -0600

    Applying rest security (keycloak)

---
+++ src/main/java/com/cydeo/service/KeycloakService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+
+import javax.ws.rs.core.Response;
+
+public interface KeycloakService {
+    Response userCreate(UserDTO dto);
+    void delete(String userName);
+}
